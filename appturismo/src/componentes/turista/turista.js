fb2bc3a3d0e23d1160020fab6c97e91fa06634bf
saimersaavedra <saimeradriansr@ufps.edu.co>
2023-05-16 12:01:58 -0500

    coso

--- appturismo/src/componentes/turista/turista.js
+++ appturismo/src/componentes/turista/turista.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import TablaTuristas from './TablaTuristas';
-
+import axios from 'axios';
 
 const Turista = () => {
+    const [list, setList] = useState([]);
+    useEffect(() => {
+
+        const fetchData = async() => {
+            try{
+                const response = await axios({
+
+                    url: "http://localhost:8080/TURISMO_1152280_POO2/api/apiturista/turista"
+                
+                });
+                setList(response.data);
+            } 
+            catch(error){
+                console.log(error);
+            }
+        };
+        fetchData();
+    });
     return (
         <div>
             <TablaTuristas/>
         </div>
     );
 };
-
 export default Turista;
