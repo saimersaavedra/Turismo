3274bb906255b03f99a13cdbe21e2076142cfe6b
BrayanBaron <brayanstivenbv@ufps.edu.co>
2023-05-25 08:47:46 -0500

    en el paquete API crear class de CorsFilter

---
+++ TURISMO_1152280_POO2/src/main/java/API/CorsFilter.java
@@ -0,0 +1,33 @@
+package API;
+
+
+import jakarta.ws.rs.container.ContainerRequestContext;
+import jakarta.ws.rs.container.ContainerResponseContext;
+import jakarta.ws.rs.container.ContainerResponseFilter;
+import jakarta.ws.rs.core.MultivaluedMap;
+import java.io.IOException;
+
+public class CorsFilter implements ContainerResponseFilter{
+
+    @Override
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
+        MultivaluedMap<String, Object> headers = responseContext.getHeaders();
+
+        // Permitir peticiones desde cualquier origen
+        headers.add("Access-Control-Allow-Origin", "*");
+
+        // Permitir los métodos HTTP permitidos
+        headers.add("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+        
+        headers.add("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+        // Agregar otros encabezados personalizados si es necesario
+        // headers.add("Access-Control-Allow-Headers", "header1, header2");
+
+        // Configurar la caché de preflight por 12 horas
+        headers.add("Access-Control-Max-Age", "43200");
+
+        // Permitir el envío de cookies y otros encabezados específicos
+        headers.add("Access-Control-Allow-Credentials", "true");
+    }
+}
--- TURISMO_1152280_POO2/src/main/java/API/RestApplication.java
+++ TURISMO_1152280_POO2/src/main/java/API/RestApplication.java
@@ -2,9 +2,19 @@ package API;
 
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
 
 
 @ApplicationPath("/api")
 public class RestApplication extends Application{
-    
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> classes = new HashSet<>();
+        classes.add(CorsFilter.class);
+        classes.add(TuristaResource.class);
+        classes.add(SitioTuristicoResource.class);
+        // Agrega aquí tus clases de recursos (endpoints) adicionales
+        return classes;
+    }
 }
